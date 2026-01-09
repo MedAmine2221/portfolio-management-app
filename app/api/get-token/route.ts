6f37068f0a42ce4268b7fe495a45a33ed02ec274
MedAmine2221 <lazregamine258@gmail.com>
2026-01-09 16:34:08 +0100

    add gif for loading page

---
+++ app/api/get-token/route.ts
@@ -0,0 +1,8 @@
+// app/api/get-token/route.ts
+import { NextResponse } from "next/server";
+import { getToken } from "@/lib/server-functions";
+
+export async function GET() {
+  const token = await getToken();
+  return NextResponse.json({ token: token ?? null });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
 "use client";
+
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  useEffect(()=>{
-    router.push("/auth")
-  },[])  
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const res = await fetch("/api/get-token");
+      const data = await res.json();
+      
+      if (data.token) {
+        router.replace("/calendar/week-view");
+      } else {
+        router.replace("/auth");
+      }
+    };
+
+    checkToken();
+  }, [router]);
+
+  return (
+    <div className="flex justify-center items-center">
+      <Image src={"/loading.gif"} width={500} height={500} alt="loading" />
+    </div>
+  )
 }
