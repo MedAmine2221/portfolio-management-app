32fdf4ea7e7df95c050c437af0f749463034840b
MedAmine2221 <lazregamine258@gmail.com>
2026-01-12 15:10:59 +0100

    change card on english

--- components/app/calendar/client-container.tsx
+++ components/app/calendar/client-container.tsx
@@ -14,13 +14,13 @@ export function ClientContainer({ view }: { view: TCalendarView }) {
       <div className="flex gap-4 justify-center flex-wrap mt-4">
         {/* Card Clients */}
         <div className="border-2 border-[#1a7ef0] rounded-xl shadow-lg bg-white p-6 flex flex-col items-center transition-transform hover:scale-105">
-          <p className="text-gray-500 text-lg mb-1 font-bold">Nombre des clients :</p>
-          <p className="text-2xl font-bold text-[#1a7ef0]">{clients.length}</p>
+          <p className="text-gray-500 text-lg mb-1 font-bold">Number of customers :</p>
+          <p className="text-2xl font-bold text-[#1a7ef0]">{clients.length > 0 ? clients.length : "pas des client" }</p>
         </div>
 
         {/* Card Projets */}
         <div className="border-2 border-[#1a7ef0] rounded-xl shadow-lg bg-white p-6 flex flex-col items-center transition-transform hover:scale-105">
-          <p className="text-gray-500 text-lg mb-1 font-bold">Nombre des projets réalisés :</p>
+          <p className="text-gray-500 text-lg mb-1 font-bold">Number of projects completed :</p>
           <p className="text-2xl font-bold text-[#1a7ef0]">9</p>
         </div>
       </div>
