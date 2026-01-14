8f18eaebdb27cd4edf4e0452c53dcec8f4acf3af
MedAmine2221 <lazregamine258@gmail.com>
2026-01-14 17:24:55 +0100

    remove unused api

--- config/nextAuth.ts
+++ config/nextAuth.ts
@@ -1,4 +1,3 @@
-import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -39,6 +38,3 @@ export const authOptions = {
     },
   },
 };
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
