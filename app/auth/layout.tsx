481c7c23685259fa472e1f89a6ac704807b387bb
MedAmine2221 <lazregamine258@gmail.com>
2026-01-16 13:22:51 +0100

    fix deployement error

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,8 +1,8 @@
-import { ChildrenProps } from "@/types/interfaces";
-
 export default function AuthLayout({
   children,
-}: ChildrenProps) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="min-h-screen flex items-center justify-center">
       {children}
