032db9dabd0219897504f797bdcf1a6499dba704
MedAmine2221 <lazregamine258@gmail.com>
2026-01-18 14:02:55 +0100

    fix problems

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -3,11 +3,17 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   const cookieStore = cookies();
+  const allCookies = (await cookieStore).getAll();
 
-  (await cookieStore).getAll().forEach(async (cookie) => {
+  allCookies.forEach(async (cookie) => {
     (await cookieStore).set(cookie.name, "", {
       path: "/",
       expires: new Date(0),
+      secure: true,
+      sameSite: "lax",
+      domain: process.env.NODE_ENV === "production"
+        ? ".vercel.app"
+        : "localhost",
     });
   });
 
